285dbaf0f5a3312f704265749764affc0af5bf2c
Min Ni <mni37@gatech.edu>
2019-11-13 15:00:23 -0500

    update

--- app/src/js/query.js
+++ app/src/js/query.js
@@ -24,42 +24,11 @@
 
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
-          // var gender = patient.gender;
-          //
-          // var fname = '';
-          // var lname = '';
-          //
-          // if (typeof patient.name[0] !== 'undefined') {
-          //   fname = patient.name[0].given.join(' ');
-          //   lname = patient.name[0].family.join(' ');
-          // }
 
           var heart_rate = byCodes('8867-4');
-          // var weight = byCodes('29463-7');
-          // var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          // var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          // var hdl = byCodes('2085-9');
-          // var ldl = byCodes('2089-1');
 
           var p = defaultPatient();
-          // p.birthdate = patient.birthDate;
-          // p.gender = gender;
-          // p.fname = fname;
-          // p.lname = lname;
-          // p.height = getQuantityValueAndUnit(height[0]);
-          // p.weight = getQuantityValueAndUnit(weight[0]);
-          p.heart_rate = getQuantityValueAndUnit(heart_rate[0]);
-
-          // if (typeof systolicbp != 'undefined')  {
-          //   p.systolicbp = systolicbp;
-          // }
-          //
-          // if (typeof diastolicbp != 'undefined') {
-          //   p.diastolicbp = diastolicbp;
-          // }
-          //
-          // p.hdl = getQuantityValueAndUnit(hdl[0]);
-          // p.ldl = getQuantityValueAndUnit(ldl[0]);
+          p.heart_rate = getQuantityValue(heart_rate[0]);
 
           ret.resolve(p);
         });
@@ -75,44 +44,44 @@
 
   function defaultPatient(){
     return {
-      // fname: {value: ''},
-      // lname: {value: ''},
-      // gender: {value: ''},
-      // birthdate: {value: ''},
-      // height: {value: ''},
-      // weight: {value: ''},
-      // systolicbp: {value: ''},
-      // diastolicbp: {value: ''},
-      // ldl: {value: ''},
-      // hdl: {value: ''},
       heart_rate: {value: ''},
     };
   }
 
-  function getQuantityValueAndUnit(ob) {
+  // function getQuantityValueAndUnit(ob) {
+  //   if (typeof ob != 'undefined' &&
+  //       typeof ob.valueQuantity != 'undefined' &&
+  //       typeof ob.valueQuantity.value != 'undefined' &&
+  //       typeof ob.valueQuantity.unit != 'undefined') {
+  //         return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
+  //   } else {
+  //     return undefined;
+  //   }
+  // }
+
+  function getQuantityValue(ob) {
     if (typeof ob != 'undefined' &&
         typeof ob.valueQuantity != 'undefined' &&
-        typeof ob.valueQuantity.value != 'undefined' &&
-        typeof ob.valueQuantity.unit != 'undefined') {
-          return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
+        typeof ob.valueQuantity.value != 'undefined') {
+          return ob.valueQuantity.value;
     } else {
       return undefined;
     }
   }
 
+  function getDateValue(ob) {
+    if (typeof ob != 'undefined' &&
+        typeof ob.effectiveDateTime != 'undefined') {
+          return ob.effectiveDateTime;
+    } else {
+      return undefined;
+    }
+  }
+
+
   window.drawVisualization = function(p) {
     $('#holder').show();
     $('#loading').hide();
-    // $('#fname').html(p.fname);
-    // $('#lname').html(p.lname);
-    // $('#gender').html(p.gender);
-    // $('#birthdate').html(p.birthdate);
-    // $('#height').html(p.height);
-    // $('#weight').html(p.weight);
-    // $('#systolicbp').html(p.systolicbp);
-    // $('#diastolicbp').html(p.diastolicbp);
-    // $('#ldl').html(p.ldl);
-    // $('#hdl').html(p.hdl);
     $('#heart_rate').html(p.heart_rate);
   };
 
