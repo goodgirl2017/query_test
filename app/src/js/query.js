220173b04296c05961acc4514a69dd7a522db311
Min Ni <mni37@gatech.edu>
2019-11-12 20:59:14 -0500

    update

--- app/src/js/query.js
+++ app/src/js/query.js
@@ -4,26 +4,26 @@
 // document.getElementById("holder").style.display = "none";
 // document.getElementById("loading").style.display = "block";
 
-// function search(){
+function search(){
 
-    // var startDay = $('#start').val();
-    //
-    // var endDay =$('#end').val();
-    //
-    // // the start date must be a valid date
-	  // if (!isValidDate(startDay)){
-		//    window.alert("Start Date is not valid");}
-    //
-    // // the end date must be a valid date
-    // else if (!isValidDate(endDay)){
-  	//    window.alert("End Date is not valid");}
-    //
-    // // the start date must be no later than end date
-    // else if (Date.parse(startDay) > Date.parse(endDay)){
-    //   window.alert("End Date should no earlier than start Date");}
+    var startDay = $('#start').val();
 
-    // both the query start and end dates are valid
-	  // else {
+    var endDay =$('#end').val();
+
+    // the start date must be a valid date
+	  if (!isValidDate(startDay)){
+		   window.alert("Start Date is not valid");}
+
+    // the end date must be a valid date
+    else if (!isValidDate(endDay)){
+  	   window.alert("End Date is not valid");}
+
+    // the start date must be no later than end date
+    else if (Date.parse(startDay) > Date.parse(endDay)){
+      window.alert("End Date should no earlier than start Date");}
+
+    both the query start and end dates are valid
+	  else {
       // TODO: https://stackoverflow.com/questions/11609376/share-data-between-html-pages
 
       // var patientId = sessionstorage.getItem('patientId')
@@ -119,7 +119,7 @@
 
 })(window);
 	// }
-// }
+}
 
 function toggle(id){
   var tb=document.getElementById(id);
@@ -129,34 +129,34 @@ function toggle(id){
 
 // Validates that the input string is a valid date formatted as "mm/dd/yyyy"
 // credits goes to https://stackoverflow.com/questions/6177975/how-to-validate-date-with-format-mm-dd-yyyy-in-javascript
-// function isValidDate(dateString)
-// {
-//     // First check for the pattern
-//     if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
-//         return false;
-//
-//     // Parse the date parts to integers
-//     var parts = dateString.split("/");
-//     var year = parseInt(parts[2], 10);
-//     var month = parseInt(parts[0], 10);
-//     var day = parseInt(parts[1], 10);
-//
-//     // get current date
-//     var today = new Date();
-//     var currentYear = today.getFullYear();
-//     var currentMonth = today.getMonth() + 1;
-//     var currentDay = today.getDate();
-//
-//     // Check the ranges of month and year
-//     if(year < 1000 || year > currentYear || month == 0 || month > 12)
-//         return false;
-//
-//     var monthDays = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
-//
-//     // Adjust for leap years
-//     if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
-//         monthDays[1] = 29;
-//
-//     // Check the range of the day
-//     return day > 0 && day <= monthDays[month - 1];
-// };
+function isValidDate(dateString)
+{
+    // First check for the pattern
+    if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+        return false;
+
+    // Parse the date parts to integers
+    var parts = dateString.split("/");
+    var year = parseInt(parts[2], 10);
+    var month = parseInt(parts[0], 10);
+    var day = parseInt(parts[1], 10);
+
+    // get current date
+    var today = new Date();
+    var currentYear = today.getFullYear();
+    var currentMonth = today.getMonth() + 1;
+    var currentDay = today.getDate();
+
+    // Check the ranges of month and year
+    if(year < 1000 || year > currentYear || month == 0 || month > 12)
+        return false;
+
+    var monthDays = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+    // Adjust for leap years
+    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+        monthDays[1] = 29;
+
+    // Check the range of the day
+    return day > 0 && day <= monthDays[month - 1];
+};
