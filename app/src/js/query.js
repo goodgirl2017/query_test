dd57dcb5ff71e971c167b935924706d591bc0bb7
Min Ni <mni37@gatech.edu>
2019-11-13 13:25:36 -0500

    separate js files

--- app/src/js/query.js
+++ app/src/js/query.js
@@ -1,69 +1,119 @@
-// assume the userid is stored at sessionstorage as userId:
-// https://stackoverflow.com/questions/11609376/share-data-between-html-pages:
-// https://healthservices.atlassian.net/wiki/spaces/HSPC/pages/64585802/How+to+Get+the+Current+User
-// document.getElementById("holder").style.display = "none";
-// document.getElementById("loading").style.display = "block";
+(function(window){
+  window.extractData = function() {
+    var ret = $.Deferred();
 
+    function onError() {
+      console.log('Loading error', arguments);
+      ret.reject();
+    }
 
-// Validates that the input string is a valid date formatted as "mm/dd/yyyy"
-// credits goes to https://stackoverflow.com/questions/6177975/how-to-validate-date-with-format-mm-dd-yyyy-in-javascript
+    function onReady(smart)  {
+      if (smart.hasOwnProperty('patient')) {
+        var patient = smart.patient;
+        var pt = patient.read();
+        var obv = smart.patient.api.fetchAll({
+                    type: 'Observation',
+                    query: {
+                      code: {
+                        $or: ['http://loinc.org|8867-4']
+                      }
+                    }
+                  });
 
+        $.when(pt, obv).fail(onError);
 
-function search(){
+        $.when(pt, obv).done(function(patient, obv) {
+          var byCodes = smart.byCodes(obv, 'code');
+          // var gender = patient.gender;
+          //
+          // var fname = '';
+          // var lname = '';
+          //
+          // if (typeof patient.name[0] !== 'undefined') {
+          //   fname = patient.name[0].given.join(' ');
+          //   lname = patient.name[0].family.join(' ');
+          // }
 
-    var startDay = $('#start').val();
+          var heart_rate = byCodes('8867-4');
+          // var weight = byCodes('29463-7');
+          // var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
+          // var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          // var hdl = byCodes('2085-9');
+          // var ldl = byCodes('2089-1');
 
-    var endDay =$('#end').val();
+          var p = defaultPatient();
+          // p.birthdate = patient.birthDate;
+          // p.gender = gender;
+          // p.fname = fname;
+          // p.lname = lname;
+          // p.height = getQuantityValueAndUnit(height[0]);
+          // p.weight = getQuantityValueAndUnit(weight[0]);
+          p.heart_rate = getQuantityValueAndUnit(heart_rate[0]);
 
-    // window.alert("line108");
+          // if (typeof systolicbp != 'undefined')  {
+          //   p.systolicbp = systolicbp;
+          // }
+          //
+          // if (typeof diastolicbp != 'undefined') {
+          //   p.diastolicbp = diastolicbp;
+          // }
+          //
+          // p.hdl = getQuantityValueAndUnit(hdl[0]);
+          // p.ldl = getQuantityValueAndUnit(ldl[0]);
 
-    checkInputValidation(startDay, endDay);
+          ret.resolve(p);
+        });
+      } else {
+        onError();
+      }
+    }
 
-}
+    FHIR.oauth2.ready(onReady, onError);
+    return ret.promise();
 
-function isValidDate(dateString)
-{
-    // First check for the pattern
-    if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
-        return false;
+  };
 
-    // Parse the date parts to integers
-    var parts = dateString.split("/");
-    var year = parseInt(parts[2], 10);
-    var month = parseInt(parts[0], 10);
-    var day = parseInt(parts[1], 10);
+  function defaultPatient(){
+    return {
+      // fname: {value: ''},
+      // lname: {value: ''},
+      // gender: {value: ''},
+      // birthdate: {value: ''},
+      // height: {value: ''},
+      // weight: {value: ''},
+      // systolicbp: {value: ''},
+      // diastolicbp: {value: ''},
+      // ldl: {value: ''},
+      // hdl: {value: ''},
+      heart_rate: {value: ''},
+    };
+  }
 
-    // get current date
-    var today = new Date();
-    var currentYear = today.getFullYear();
-    var currentMonth = today.getMonth() + 1;
-    var currentDay = today.getDate();
+  function getQuantityValueAndUnit(ob) {
+    if (typeof ob != 'undefined' &&
+        typeof ob.valueQuantity != 'undefined' &&
+        typeof ob.valueQuantity.value != 'undefined' &&
+        typeof ob.valueQuantity.unit != 'undefined') {
+          return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
+    } else {
+      return undefined;
+    }
+  }
 
-    // Check the ranges of month and year
-    if(year < 1000 || year > currentYear || month == 0 || month > 12)
-        return false;
+  window.drawVisualization = function(p) {
+    $('#holder').show();
+    $('#loading').hide();
+    // $('#fname').html(p.fname);
+    // $('#lname').html(p.lname);
+    // $('#gender').html(p.gender);
+    // $('#birthdate').html(p.birthdate);
+    // $('#height').html(p.height);
+    // $('#weight').html(p.weight);
+    // $('#systolicbp').html(p.systolicbp);
+    // $('#diastolicbp').html(p.diastolicbp);
+    // $('#ldl').html(p.ldl);
+    // $('#hdl').html(p.hdl);
+    $('#heart_rate').html(p.heart_rate);
+  };
 
-    var monthDays = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
-
-    // Adjust for leap years
-    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
-        monthDays[1] = 29;
-
-    // Check the range of the day
-    return day > 0 && day <= monthDays[month - 1];
-};
-
-function checkInputValidation(startDay, endDay) {
-
-  // the start date must be a valid date
-  if (!isValidDate(startDay)){
-     window.alert("Start Date is not valid");}
-
-  // the end date must be a valid date
-  else if (!isValidDate(endDay)){
-     window.alert("End Date is not valid");}
-
-  // the start date must be no later than end date
-  else if (Date.parse(startDay) > Date.parse(endDay)){
-    window.alert("End Date should no earlier than start Date");}
-};
+})(window);
